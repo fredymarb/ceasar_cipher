38851dd45b8be1376d8936e31df1c55d7da94631
Fredrick Marbell <29468132+fredymarb@users.noreply.github.com>
2024-07-26 10:53:34 +0000

    add comments to help understand function

--- script.rb
+++ script.rb
@@ -1,16 +1,18 @@
 def ceasar_cipher(string, shift_factor)
+  # ensure shift_factor is within 0 - 26 range
   shift_factor = shift_factor % 26
 
   encrypted_string = ""
 
   string.each_char do |char|
-    if char.match?(/[a-z]/)
+    if char.match?(/[a-z]/) # shift lowercase alphabets
       new_char = (((char.ord - "a".ord + shift_factor) % 26) + "a".ord).chr
       encrypted_string += new_char
-    elsif char.match?(/[A-Z]/)
+    elsif char.match?(/[A-Z]/) # shift uppercase alphabets
       new_char = (((char.ord - "A".ord + shift_factor) % 26) + "A".ord).chr
       encrypted_string += new_char
     else
+      # leave non-alphabet character untouched
       encrypted_string += char
     end
   end
